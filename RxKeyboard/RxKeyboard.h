dce01ec49982a096768fe80270c562287083d066
Vadim Balashov <balashov@corp.mail.ru>
2020-05-07 22:49:12 +0300

    carthage support

---
+++ RxKeyboard/RxKeyboard.h
@@ -0,0 +1,19 @@
+//
+//  RxKeyboard.h
+//  RxKeyboard
+//
+//  Created by Вадим Балашов on 08.05.2020.
+//  Copyright © 2020 RxSwiftCommunity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxKeyboard.
+FOUNDATION_EXPORT double RxKeyboardVersionNumber;
+
+//! Project version string for RxKeyboard.
+FOUNDATION_EXPORT const unsigned char RxKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxKeyboard/PublicHeader.h>
+
+
